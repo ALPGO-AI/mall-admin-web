286c09d60b5566952b6aa672835f7d19c5fa31fc
Marcus Zhang <marcuszhang@augmentum.com.cn>
2023-07-11 16:17:13 +0800

    feat: update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <router-view/>
+    <div class="footer">
+      <span>© 2023 ALPGO. All Rights Reserved. | <a href="https://beian.miit.gov.cn/" target="_blank">湘ICP备2021021685号-1</a></span>
+    </div>
   </div>
 </template>
 
@@ -11,4 +14,23 @@
 </script>
 
 <style>
+.footer {
+  background-color: #f2f2f2;
+  padding: 20px;
+  text-align: center;
+}
+
+.footer span {
+  font-size: 12px;
+  color: #777777;
+}
+
+.footer a {
+  color: #777777;
+  text-decoration: none;
+}
+
+.footer a:hover {
+  text-decoration: underline;
+}
 </style>
